e2bd208cee1e05e90bfa6f6d2e21e30d06079903
IgorSilva89 <igor_ea@yahoo.com.br>
2021-02-23 17:24:29 -0300

    Add script para criar e deletar a migration

--- src/database/migrations/1614108005505-CreateUser.ts
+++ src/database/migrations/1614108005505-CreateUser.ts
@@ -1,11 +1,45 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateUser1614108005505 implements MigrationInterface {
 
+    /**
+     * Quando eu quero dar um create na migration que estou executando
+     * @param queryRunner 
+     */
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                    },
+                    {
+                        name: "email",
+                        type: "varchar",
+                    },
+                    {
+                        name: "create_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                ]
+            })
+        )
     }
 
+    /**
+     * Quando eu quero dar um drop na migration que estou executando
+     * @param queryRunner 
+     */
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users");
     }
 
 }
