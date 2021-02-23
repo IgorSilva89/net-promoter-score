8b17f77e9cb2c3150b4b25ac50efdba9d666b553
IgorSilva89 <igor_ea@yahoo.com.br>
2021-02-23 19:03:53 -0300

    Limpeza de codigo

--- src/server.ts
+++ src/server.ts
@@ -1,44 +1,12 @@
 import 'reflect-metadata';
 import express from 'express';
 import './database/index';
+import { router } from './routes';
 
 const app = express();
-
-/**
- * GET => Buscar
- * POST => Salvar
- * PUT => Alterar
- * DELETE => Deletar
- * PATCH => Alteração específica
- */
-
-/**
- * http://localhost:3333/users
- * 1 param => Rota (Recurso API)
- * 2 param => request, response
- */
-app.get("/", (request, response) => {
-    // return response.send('Requisição feita com sucesso');
-    return response.json({ message: 'Requisição get feita com sucesso' });
-})
-
-app.post("/", (request, response) => {
-    return response.json({ message: 'Requisição post feita com sucesso' });
-})
-
-app.put("/", (request, response) => {
-    return response.json({ message: 'Requisição put feita com sucesso' });
-})
-
-app.delete("/", (request, response) => {
-    return response.json({ message: 'Requisição delete feita com sucesso' });
-})
-
-app.patch("/", (request, response) => {
-    return response.json({ message: 'Requisição patch feita com sucesso' });
-})
-
-
+app.use(express.urlencoded({ extended: true })) 
+app.use(express.json());
+app.use(router);
 /**
  * Esta opção cria o servidor. Importante lembrar de setar uma porta que não esteja sendo utilizada
  */
