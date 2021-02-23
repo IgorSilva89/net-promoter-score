9edd5ebf5b7f09005873c25e082bcb31b5080df0
IgorSilva89 <igor_ea@yahoo.com.br>
2021-02-23 16:21:49 -0300

    Add migration CreateUser

---
+++ src/database/migrations/1614108005505-CreateUser.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateUser1614108005505 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
