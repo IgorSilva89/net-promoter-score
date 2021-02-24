a3cd64dea3c9b3f9176f894e0f514e0fa4e80bd1
IgorSilva89 <igor_ea@yahoo.com.br>
2021-02-24 17:49:37 -0300

    Add teste de negacao

--- src/__tests__/First.test.ts
+++ src/__tests__/First.test.ts
@@ -6,4 +6,8 @@ describe("First", () => {
     it("deve ser possível somar 2 números", () => {
         expect(2 + 2).toBe(5);
     });
+
+    it("deve ser possível somar 2 números", () => {
+        expect(2 + 2).not.toBe(5);
+    });
 });
